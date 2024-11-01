5544d947a5fca9aef0a3c1c1cacc9b73cc393acc
prabin <prabinkumarsahu28@gmail.com>
2024-11-01 22:14:37 +0530

    langchain_openai in install_requires

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Prabin Kumar Sahu',
     author_email='prabinkumarsahu28@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["openai", "langchain", "langchain_openai", "streamlit", "python-dotenv", "PyPDF2"],
     packages=find_packages()
 )
